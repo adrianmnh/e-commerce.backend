d6691499148485d1df66f39893fbd393c97319d1
Adrian Legion <nlawliet6@gmail.com>
2024-01-31 00:45:04 -0500

    Add schema and route for creating products

--- index.js
+++ index.js
@@ -48,6 +48,143 @@ app.post("/uploads", upload.single("product"), ( req, res ) => {
 });
 
 
+
+// Schema for Creating Products
+
+const Product = mongoose.model("Product", {
+	id : {
+		type: Number,
+		required: false,
+	},
+	name : {
+		type: String,
+		required: true,
+	},
+	image : {
+		type: String,
+		required: true,
+	},
+	category : {
+		type: String,
+		required: true,
+	},
+	new_price : {
+		type: Number,
+		required: true,
+	},
+	old_price : {
+		type: Number,
+		required: true,
+	},
+	date : {
+		type: Date,
+		default: Date.now
+	},
+	available : {
+		type: Boolean,
+		default: true,
+	}
+
+});
+
+// const CounterSchema = new mongoose.Schema({
+// 	_id: { db: String, coll: String },
+// 	seq_value: Number,
+//   });
+
+//   const Counter = mongoose.model('Counter', CounterSchema);
+
+// app.post("/addproduct", async (req, res) => {
+// 	try {
+// 		// Get the current value of seq_value from the counters collection
+// 		// const counter = await Counter.findOneAndUpdate(
+// 		// 	{ _id: { db: "e-commerce", coll: "products" } },
+// 		// 	{ $inc: { seq_value: 1 } },
+// 		// 	{ new: true }
+// 		// );
+
+// 		const product = new Product({
+// 			// id: counter.seq_value,
+
+// 			name: req.body.name,
+// 			image: req.body.image,
+// 			category: req.body.category,
+// 			new_price: req.body.new_price,
+// 			old_price: req.body.old_price,
+// 			date: req.body.date,
+// 			available: req.body.available,
+// 		});
+
+// 		// Save the product to the database
+// 		await product.save();
+
+// 		res.json({
+// 			success: 1,
+// 			message: "Product added successfully",
+// 			id: product.id,
+// 		});
+// 	} catch (error) {
+// 		res.status(500).json({
+// 			success: 0,
+// 			message: "Failed to add product",
+// 			error: error.message,
+// 		});
+// 	}
+// });
+
+
+
+app.post("/addproduct", async( req, res ) => {
+
+	let products = await Product.find({});
+
+	let id;
+	if(products.length > 0) {
+		let last_product_array = products.slice(-1);
+		let last_product = last_product_array[0];
+		id = last_product.id + 1;
+	} else {
+		id = 1;
+	}
+
+	const product = new Product({
+		// id: req.body.id,
+		id: id,
+		name: req.body.name,
+		image: req.body.image,
+		category: req.body.category,
+		new_price: req.body.new_price,
+		old_price: req.body.old_price,
+	});
+
+	console.log(product);
+	await product.save();
+	console.log("Saved");
+	res.json({
+		success: 1,
+		generated_id: product._id,
+		id: product.id,
+		name: product.name,
+	})
+})
+
+// app.delete("/deleteallproducts", async (req, res) => {
+// 	try {
+// 	  await Product.deleteMany({});
+// 	  res.json({
+// 		success: 1,
+// 		message: "All products deleted successfully",
+// 	  });
+// 	} catch (error) {
+// 	  res.status(500).json({
+// 		success: 0,
+// 		message: "Failed to delete products",
+// 		error: error.message,
+// 	  });
+// 	}
+//   });
+
+
 app.listen(port, (error) => {
 	if(!error) {
 		console.log(`Server is running on port: ${port}`);
